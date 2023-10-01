47fae4c95f4219a8419bdf8598f9d7b51e49fdee
Gayashan <gayashan333madhuka99@gmail.com>
2023-10-01 21:03:05 +1100

    initializing student details

--- Assignment2/Student.java
+++ Assignment2/Student.java
@@ -17,4 +17,16 @@ class Student {
     double assignmentNo2; 
     double assignmentNo3; 
     double studentTotalMark;
+    
+    // Initializing student details
+    public Student(String studentLastName, String studentFirstName, int studentID, 
+    double assignmentNo1, double assignmentNo2, double assignmentNo3) {
+        this.studentLastName = studentLastName;
+        this.studentFirstName = studentFirstName;
+        this.studentID = studentID;
+        this.assignmentNo1 = assignmentNo1;
+        this.assignmentNo2 = assignmentNo2;
+        this.assignmentNo3 = assignmentNo3;
+        this.studentTotalMark = assignmentNo1 + assignmentNo2 + assignmentNo3;
+    }
 }
